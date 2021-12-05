409b547a5859b02faa0a93faeac665d38c133303
eneserdo1 <eneserdogan.66@outlook.com>
2021-12-05 20:18:18 +0300

    Added dispatchers

--- app/src/main/java/com/app/imkbapp/data/repository/RemoteRepository.kt
+++ app/src/main/java/com/app/imkbapp/data/repository/RemoteRepository.kt
@@ -5,8 +5,10 @@ import com.app.imkbapp.model.Detail.StockDetailResponse
 import com.app.imkbapp.model.Handshake.HandShakeResponse
 import com.app.imkbapp.model.Resource
 import com.app.imkbapp.model.Stocks.StocksResponse
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
 import  kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemoteRepository @Inject constructor(val remoteDataSource:RemoteDataSource){
@@ -14,20 +16,20 @@ class RemoteRepository @Inject constructor(val remoteDataSource:RemoteDataSource
     suspend fun fetchHandShake(bodyMap:Map<String,String>) :Flow<Resource<HandShakeResponse>>{
         return flow {
             emit(remoteDataSource.getHandShake(bodyMap))
-        }
+        }.flowOn(Dispatchers.IO)
     }
 
 
     suspend fun fetchStockList(bodyMap:Map<String,String>,token:String) :Flow<Resource<StocksResponse>>{
         return flow {
             emit(remoteDataSource.getStocks(bodyMap,token))
-        }
+        }.flowOn(Dispatchers.IO)
     }
 
 
     suspend fun fetchStockDetail(bodyMap:Map<String,String>,token:String) :Flow<Resource<StockDetailResponse>>{
         return flow {
             emit(remoteDataSource.getStockDetail(bodyMap,token))
-        }
+        }.flowOn(Dispatchers.IO)
     }
 }
